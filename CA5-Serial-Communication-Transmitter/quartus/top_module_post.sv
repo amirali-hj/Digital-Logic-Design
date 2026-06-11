7ea0af441d89aac6976462ce28d238e96b0fce9b
Amirali Hajizadeh Moghaddam <hajizadehamirali@gmail.com>
2026-06-11 20:31:10 +0330

    Add files via upload

---
+++ CA5-Serial-Communication-Transmitter/quartus/top_module_post.sv
@@ -0,0 +1,33 @@
+
+
+module top_module_post (
+    input  logic clk,
+    input  logic rst,
+    input  logic rcIn,
+    output logic txOut,
+    output logic txValid,
+    output logic txAbort
+);
+    logic start_detected, abort_detected;
+
+    // Start detector
+    sequence_detector u_start (
+        .clk(clk), .rst(rst), .J(~rcIn), .detected(start_detected)
+    );
+
+    // Abort detector
+    sequence_detector u_abort (
+        .clk(clk), .rst(rst), .J(rcIn), .detected(abort_detected)
+    );
+
+    // Transmitter FSM
+    transmitter_fsm u_fsm (
+        .clk(clk), .rst(rst),
+        .start(start_detected),
+        .abort(abort_detected),
+        .rcIn(rcIn),
+        .txOut(txOut),
+        .txValid(txValid),
+        .txAbort(txAbort)
+    );
+endmodule
