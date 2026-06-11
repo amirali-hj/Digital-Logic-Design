8bf0fcb0490c3a47b374df4abb345cd55bcc4ff6
Amirali Hajizadeh Moghaddam <hajizadehamirali@gmail.com>
2026-06-11 20:07:02 +0330

    Add files via upload

--- CA4-FSMD-Sequence-Detector/tb/FSM_TB.sv
+++ CA4-FSMD-Sequence-Detector/tb/FSM_TB.sv
@@ -1,1 +1,47 @@
-`
+`timescale 1ns/1ns
+module tb_sequence_detector;
+
+    logic clk, rst, J;
+    logic detected;
+
+    sequence_detector dut (
+        .clk(clk),
+        .rst(rst),
+        .J(J),
+        .detected(detected)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+
+        rst = 1; J = 0;
+        #10;
+        rst = 0;
+
+        J = 1; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 1; #10;
+
+        J = 0; #20;
+
+        J = 1; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 0; #10;
+        J = 1; #10;
+
+        #20;
+        $stop;
+    end
+
+endmodule
+
