745b1fe36151854be3da7bf04989537567d68a12
Amirali Hajizadeh Moghaddam <hajizadehamirali@gmail.com>
2026-06-16 17:32:36 +0330

    Delete Lab04-Digital-Modulation-DDS-PWM directory

--- Lab04-Digital-Modulation-DDS-PWM/src/comp2sign.sv
+++
@@ -1,8 +0,0 @@
-module comp2sign #(parameter WIDTH = 8) (
-    input  logic             mode,
-    input  logic [WIDTH-1:0] din,
-    output logic [WIDTH-1:0] dout,
-    output logic             co
-);
-    assign {co, dout} = mode ? (~{1'b0, din} + 1'b1) : {1'b0, din};
-endmodule
